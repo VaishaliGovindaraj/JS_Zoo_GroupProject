38442e2164eb46831a68dc32d263ab353cf1fc65
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2024-12-18 16:11:09 +0100

    updating filepath in reptile.js

--- scripts/reptiles.js
+++ scripts/reptiles.js
@@ -163,16 +163,16 @@ function Animals(groupName, name, lifeSpan, food, description, length, weight, p
 
 let reptileFrillNeckedLizard = new Animals("Reptiles","Frill-necked lizard", 20,["small insects", "spiders"], 
         "When this unique creature feels threatened, it rises on its hind legs, opens its yellow-coloured mouth, unfurls the colorful, pleated skin flap that encircles its head, and hisses. If an attacker is unintimidated by these antics, the lizard simply turns tail, mouth and frill open, and bolts, legs splaying left and right. It continues its deliberate run without stopping or looking back until it reaches the safety of a tree.",
-        90,1,"Northern  Austalia",  "../images/frilled_necked_lizard.jpg", 'Reptiles', '../html/Reptiles.html');
+        90,1,"Northern  Austalia",  "../images/frilled_necked_lizard.jpg", 'Reptiles', '../html/reptiles.html');
    
 let reptileHawksbillTurtle = new Animals("Reptiles","Hawksbill Turtle", 50,["sponges", "jellyfish","seaplants"], 
     "The Hawksbill Sea Turtle gets its common name from the distinctive mouth, which resembles a bird’s beak. The shell, or carapace, is covered in large overlapping scutes, which are a distinctive brown/green/amber background with lighter brown streaks throughout. At the edge of the carapace they overlap in such a way as to form a serrated edge. The turtle has an elongated head and flippers which have two visible claws on the end. The flippers and head are covered in large green, brown or yellow scales. The average adult female weighs 50 kg and their carapace (shell) is approximately 80 cm in length",
-    80,50,"Tropical coasts of Queensland, Northern Territory and Western Australia",  "../images/Hawksbill Turtle.jpg", 'Reptiles', '../html/Reptiles.html');
+    80,50,"Tropical coasts of Queensland, Northern Territory and Western Australia",  "../images/Hawksbill Turtle.jpg", 'Reptiles', '../html/reptiles.html');
 
     
 let reptilePerentie  = new Animals("Reptile","Perentie", 20,["Carnivore", "animals like kangaroos", "rabbits", "lizards and birds"], 
     ": The perentie (Varanus giganteus) is the largest monitor lizard or goanna native to Australia. It is one of the largest living lizards on earth, after the Komodo dragon, Asian water monitor, crocodile monitor, and intersecting by size with Nile monitor.[3] Found west of the Great Dividing Range in the arid areas of Australia, it is rarely seen, because of its shyness and the remoteness of much of its range from human habitation. The species is considered to be a least-concern species according to the International Union for Conservation of Nature. Its status in many Aboriginal cultures is evident in the totemic relationships, and part of the Ngiṉṯaka dreaming, as well as bush tucker. It was a favoured food item among desert Aboriginal tribes, and the fat was used for medicinal and ceremonial purposes.",
-    250,20,"Desert",  "../images/perentie.jpg", 'Reptiles', '../html/Reptiles.html');
+    250,20,"Desert",  "../images/perentie.jpg", 'Reptiles', '../html/reptiles.html');
     
 
 //add the animals into an array
